3a60852d814babf46fe400de788121e37ef68861
Mominul Islam <imomin126@gmail.com>
2018-10-23 12:55:47 +0600

    Product Information Updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -90,7 +90,10 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
